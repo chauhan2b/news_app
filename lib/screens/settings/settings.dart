2377d80f5f3a00adb4c212000bc5906436164fa4
Bharat Chauhan <allmight524@gmail.com>
2023-09-13 12:01:53 +0530

    remove shimmer example

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -14,9 +14,6 @@ class Settings extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final size = MediaQuery.of(context).size;
-    Color primaryColor = Theme.of(context).colorScheme.primary;
-
     final darkTheme = ref.watch(darkThemeStateProvider);
     final country = ref.watch(countriesStateProvider);
     return Scaffold(
@@ -52,61 +49,6 @@ class Settings extends ConsumerWidget {
           ),
           TopHeadlinesDropdown(country: country),
           const CategoryDropdown(),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            child: Shimmer.fromColors(
-              baseColor: primaryColor.withOpacity(0.05),
-              highlightColor: primaryColor.withOpacity(0.2),
-              child: Column(
-                children: [
-                  Container(
-                    height: size.height * 0.24,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Container(
-                    height: 24,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(6),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        height: 24,
-                        width: 160,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(6),
-                        ),
-                      ),
-                      const Spacer(),
-                      Container(
-                        height: 24,
-                        width: 24,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
         ],
       ),
     );
