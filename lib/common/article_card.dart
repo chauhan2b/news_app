9b612a42378e5d5ed8710a224a22d01b65e37e87
Bharat Chauhan <allmight524@gmail.com>
2023-09-19 15:14:06 +0530

    change from news to article

--- lib/common/article_card.dart
+++ lib/common/article_card.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:news_app/models/news.dart';
+import 'package:news_app/models/article.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
 class ArticleCard extends ConsumerWidget {
-  final News article;
+  final Article article;
   const ArticleCard({super.key, required this.article});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final size = MediaQuery.of(context).size;
     Color primaryColor = Theme.of(context).colorScheme.primary;
-    final dateTime = DateTime.now().difference(article.publishedAt);
+    final dateTime = DateTime.now().difference(article.publishedDate);
     final articleDate = DateTime.now().subtract(dateTime);
     return InkWell(
       onTap: () async {
         if (!await launchUrl(
-          Uri.parse(article.url),
+          Uri.parse(article.link),
           mode: LaunchMode.externalApplication,
         )) {
           throw Exception('Could not launch');
@@ -63,7 +63,7 @@ class ArticleCard extends ConsumerWidget {
                     ],
                   ),
                 ),
-                article.imageUrl,
+                article.media,
                 fit: BoxFit.cover,
                 height: size.height * 0.24,
                 width: double.infinity,
@@ -84,7 +84,7 @@ class ArticleCard extends ConsumerWidget {
               children: [
                 Expanded(
                   child: Text(
-                    '${article.source} | ${timeago.format(articleDate, locale: 'en_short')}',
+                    '${article.cleanUrl} | ${timeago.format(articleDate, locale: 'en_short')}',
                     maxLines: 1,
                     overflow: TextOverflow.ellipsis,
                   ),
@@ -93,7 +93,7 @@ class ArticleCard extends ConsumerWidget {
                   padding: EdgeInsets.zero,
                   icon: const Icon(Icons.share_outlined),
                   onPressed: () {
-                    Share.share(article.url);
+                    Share.share(article.link);
                   },
                 ),
               ],
