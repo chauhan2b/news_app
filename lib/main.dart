a0e35338b53111676a9af2d0e1fd2434921030c7
Bharat Chauhan <allmight524@gmail.com>
2023-12-05 11:25:49 +0530

    Initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:news_app/app.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // connect with firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   // keeps device in portrait mode
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
