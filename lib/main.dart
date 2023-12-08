db22316b1c81f94aaa3ec4025715c782d8b308f8
Bharat Chauhan <allmight524@gmail.com>
2023-12-08 15:33:12 +0530

    Improve comment

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // connect with firebase
+  // initialize firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
