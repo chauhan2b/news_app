4850dfe77a12255523e91e78e71f0a1a538c38bd
Bharat Chauhan <allmight524@gmail.com>
2023-12-16 21:22:50 +0530

    Dispose providers on logout

--- lib/controller/auth_sign_out_controller.dart
+++ lib/controller/auth_sign_out_controller.dart
@@ -1,3 +1,7 @@
+import 'package:news_app/providers/category_state.dart';
+import 'package:news_app/providers/country_state.dart';
+import 'package:news_app/providers/domains_state.dart';
+import 'package:news_app/providers/sort_by_state.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../repositories/auth_repository.dart';
@@ -14,6 +18,14 @@ class AuthSignOutController extends _$AuthSignOutController {
   Future<void> signOut() async {
     final authRepository = ref.read(authRepositoryProvider);
     state = const AsyncValue.loading();
+
+    // dispose all providers
+    ref.invalidate(categoryStateProvider);
+    ref.invalidate(countriesStateProvider);
+    ref.invalidate(domainsProvider);
+    ref.invalidate(sortByStateProvider);
+
+    // sign out
     state = await AsyncValue.guard(() => authRepository.signOut());
   }
 }
