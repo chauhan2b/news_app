e5a52a4b7c882750200f517f69e6f786314ec8a3
Bharat Chauhan <allmight524@gmail.com>
2024-02-12 10:32:34 -0500

    add explanation for authRegister provider

--- lib/core/features/auth/data/auth_register_provider.dart
+++ lib/core/features/auth/data/auth_register_provider.dart
@@ -2,6 +2,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'auth_register_provider.g.dart';
 
+// simple provider to decide which button to show; i.e register or login
 @riverpod
 class AuthRegister extends _$AuthRegister {
   @override
