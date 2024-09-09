7938f110f3d8c2a6723d3e8dfc9545ff9faf40ff
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-09-09 00:57:56 -0400

    Remove unnecessary files and configurations for macOS, Linux, Windows, and upgrade Android files

--- android/app/src/main/kotlin/com/example/news_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/news_app/MainActivity.kt
@@ -1,13 +1,5 @@
 package com.example.news_app
 
-import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-    override fun onPostResume() {
-        super.onPostResume()
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        window.navigationBarColor = 0 //for transparent nav bar
-        window.statusBarColor = 0 //for transparent status bar
-    }
-}
+class MainActivity: FlutterActivity()
